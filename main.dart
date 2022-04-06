0346bbd01e05bc8d0e1f3d147bd7974b746ba48b
DongHan <tnqkr98@naver.com>
2022-04-06 18:09:50 +0900

    first

---
+++ main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material Flutter App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home:MaterialFlutterApp()
+    );
+  }
+}
+
+class MaterialFlutterApp extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _MaterialFlutterApp();
+}
+
+class _MaterialFlutterApp extends State<MaterialFlutterApp>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Material Design App')),
+      floatingActionButton: FloatingActionButton(child: Icon(Icons.add),
+        onPressed: (){
+
+        },
+      ),
+      body: Container(
+        child:Center(
+            child:Column(
+              children: <Widget>[Icon(Icons.android),Text('Android')],
+              mainAxisAlignment: MainAxisAlignment.center,
+            )
+        )
+      )
+    );
+  }
+}

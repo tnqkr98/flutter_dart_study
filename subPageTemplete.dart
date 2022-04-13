f87b147556c04a66b5122f9a920bc0a6b441eba9
Dong Han Park <tnqkr98@naver.com>
2022-04-13 16:35:09 +0900

    Practice - Navigator , Route

---
+++ subPageTemplete.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SubPage Example',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => FirstPage(),
+        '/second': (context) => SecondPage()
+      },
+    );
+  }
+}
+
+class FirstPage extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() => _FirstPage();
+}
+
+class _FirstPage extends State<FirstPage>{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sub Page Main'),
+      ),
+      body: Container(
+        child: Center(
+          child: Text('첫 번째 페이지'),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          //Navigator.of(context).push(MaterialPageRoute(builder: (context) => SecondPage()));
+          Navigator.of(context).pushNamed('/second');
+        },
+        child: Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Container(
+        child: Center(
+          child: RaisedButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: Text('돌아가기'),
+          ),
+        ),
+      )
+    );
+  }
+}
